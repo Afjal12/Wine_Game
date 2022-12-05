dae42f086c01d4d8644dbb70bd03210298c84bc2
Afjal12 <afjal.khan@speqto.com>
2022-12-05 18:07:29 +0530

    speqto

--- src/Components/Index2/YourTransaction.js
+++ src/Components/Index2/YourTransaction.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { useEffect } from 'react';
 import DataTable, { createTheme } from 'react-data-table-component';
 import { Web3WalletContext } from '../../Context/UseContext';
 import { ContractFunctionsContext } from '../Utils/ContractFunctions';
@@ -7,38 +8,44 @@ export default function YourTransaction() {
     const columns = [
         {
             name: 'Address',
-            selector: row => row.user.slice(0,10)+'...'
+            selector: row => row.user.slice(0, 10) + '...',
+            sortable: true,
         },
         {
             name: 'Bet Tokens',
-            selector: row => row.BetTokens.toString()
+            selector: row => row.BetTokens.toString(),
+            sortable: true,
         },
         {
             name: 'Benefits',
-            selector: row => row.TimesBenefit.toString()
+            selector: row => row.TimesBenefit.toString(),
+            sortable: true,
         },
         {
             name: 'Date',
             selector: row => {
                 let dt = new Date(row.BetTime.toString() * 1000).toDateString();
                 return dt
-            }
+            },
+            sortable: true,
         },
         {
-            name : 'Head or Tail',
-            selector : row => row._HeadOrTail
+            name: 'Head or Tail',
+            selector: row => row._HeadOrTail,
+            sortable: true,
         },
         {
-            name : 'Result',
-            selector : (row) => {
+            name: 'Result',
+            selector: (row) => {
                 if (row._HeadOrTail == 'Head' && row.result == true || row._HeadOrTail == 'Tail' && row.result == false) {
-                   return 'Head' 
+                    return 'Head'
                 }
                 else if (row._HeadOrTail == 'Tail' && row.result == true || row._HeadOrTail == 'Head' && row.result == false) {
                     return 'Tail'
                 }
 
-            }
+            },
+            sortable: true,
         }
 
     ];
@@ -79,6 +86,14 @@ export default function YourTransaction() {
     let filter = transaction.filter((e) => {
         return e.user.toString().toLowerCase() === accountAddress.toLowerCase()
     })
+
+    useEffect(() => {
+        let c = Math.round(100)
+        console.log(c);
+        let check = Math.floor(Math.random() * 100)
+        console.log(check);
+    }, [])
+
     return (
         <>
             <div className="pagetitle  text-light" style={{ background: '#072333', color: 'rgba(255, 255, 255, 0.781)' }}>
